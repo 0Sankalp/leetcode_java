77040c8ff06f4bced79689ea06d909c39eefbb6d
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2026-06-26 23:21:31 +0530

    Time: 17 ms (79.64%), Space: 49.9 MB (16.37%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);   // Step 1: sort array
+        
+        int sum = 0;
+
+        // Step 2: add every alternate element
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += nums[i];
+        }
+
+        return sum;
+    }
+}
